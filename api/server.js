08fa1af099c82818556eaf35c5266ab8d586bd33
Tal <talvindersingh1992@gmail.com>
2021-03-01 22:58:23 +0530

    changed path

--- api/server.js
+++ api/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.get('*', (req, res) => {
       console.log('prd');
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
   });
 }
 
