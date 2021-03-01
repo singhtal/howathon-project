58328c015c2b9740f3688494cbd438a7fbde5fc3
Tal <talvindersingh1992@gmail.com>
2021-03-01 23:15:34 +0530

    build2

--- api/server.js
+++ api/server.js
@@ -31,16 +31,15 @@ app.use("/skills", SkillsRoutes);
 app.use("/rating", ratingRoutes);
 app.use("/dashboard", dashboardRoutes);
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('../build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-      console.log('prd');
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build2','index.html'));
   });
-}
+// }
 
 module.exports = app
