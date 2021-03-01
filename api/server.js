762508985807119fd4816c5472f148d01e0553e3
Tal <talvindersingh1992@gmail.com>
2021-03-01 22:31:55 +0530

    added route

--- api/server.js
+++ api/server.js
@@ -31,4 +31,15 @@ app.use("/skills", SkillsRoutes);
 app.use("/rating", ratingRoutes);
 app.use("/dashboard", dashboardRoutes);
 
+   if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '', 'build', 'index.html'));
+  });
+}
+
 module.exports = app
